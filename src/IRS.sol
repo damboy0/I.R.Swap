18b5be85a32ff8d53c14f7114b987bf09b3d8499
damboy0 <fagbolade006@gmail.com>
2026-03-31 07:36:46 +0100

    add: createSwap function logic

--- src/IRS.sol
+++ src/IRS.sol
@@ -1,33 +1,72 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.2.27;
+
 contract InterestBasesSwap {
 
-    uint constant BPS = 10,000;
-
+    // Constants 
+    uint256 constant BPS = 10_000;
+    uint256 constant YEAR = 365 days;
 
     enum State {
         Active,
-        notActive,
+        Settled ,
         Cancelled
     }
 
-    // parties involved
-    address fixedRatePayer; // address paying the fixed rate 
-    address floatingRatePayer; // address paying the floating rate 
+    struct Swap {
+        // parties involved
+        address fixedRatePayer; // address paying the fixed rate
+        address floatingRatePayer; // address paying the floating rate
+        //Terms Involved
+        uint256 referenceAmount; // also notional . The reference number used purely for calculating interest.
+        uint256 fixedRate;
+        uint256 floatingRate;
+        //Time Taken
+        uint256 swapBegin; // when tje swap begins
+        uint256 swapDuration; // Duration of the swap
+        //
+        State state;
+    }
 
-    //Terms Involved
-    uint256 referenceAmount;
-    uint256 fixedRate;
-    uint256 floatingRate;
+    uint256 public swapCount;
 
-    //Time Taken 
-    uint256 swapBegin; // when tje swap begins
-    uint256 swapDuration; // Duration of the swap  
-
-    //
-    State state; 
+    mapping (uint256 => Swap) public swaps;
+    mapping (uint256 => mapping (address => uint256)) public deposits;
 
 
+    //Events
+    event SwapCreated(uint256 indexed id, address fixedRatePayer, address floatingRatePayer);
 
-    constructor() {
-        
+    //Errors
+    error insufficientCollateral();
+    
+
+
+    function createSwap(
+        address floatingRatePayer, 
+        uint256 referenceAmount,
+        uint256 fixedRate,
+        uint256 floatingRate,
+        uint256 duration
+    )  external payable returns (uint256 swapId) {
+        if (msg.value == 0) revert insufficientCollateral();
+
+        swapId = swapCount++;
+
+        swaps[swapId] = Swap({
+            fixedRatePayer : msg.sender,
+            floatingRatePayer: floatingRatePayer,
+            referenceAmount: referenceAmount,
+            fixedRate: fixedRate,
+            floatingRate: floatingRate,
+            swapBegin: block.timestamp, 
+            swapDuration: duration,
+            state: State.Active
+        });
+
+        deposits[swapId][msg.sender] = msg.value; 
+
+        emit SwapCreated( swapId , msg.sender, floatingRatePayer );
+
     }
 }
